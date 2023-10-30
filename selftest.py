d8c828db1c3092040ea9102088a5a8a2eaa49326
WINTERNATIONNN <dou6@wisc.edu>
2023-10-30 16:01:32 +0800

    with montly

--- selftest.py
+++ selftest.py
@@ -4,22 +4,17 @@ import datetime
 from datetime import date
 import json
 
-f = open('test.json',)
-iflow_expression= json.loads(f.read())["iflow_list"]
-# descriptor = ExpressionDescriptor(
-#     expression =  "*/5 15 * * THU",
-#     casing_type = CasingTypeEnum.Sentence,
-#     use_24hour_time_format = True
-# )
-list = []
+EXPRESSION = "*/5 15 * * THU"
+descriptor = ExpressionDescriptor(
+    expression = EXPRESSION,
+    casing_type = CasingTypeEnum.Sentence,
+    use_24hour_time_format = True
+)
+sched = ExpressionSchedular(EXPRESSION, qt = datetime.date(2023,10,26))
+print(descriptor.get_description(DescriptionTypeEnum.FULL))
+print([i.strftime("%H:%M:%S") for i in sched.get_schedule_timetable()])
 
-for iflow in iflow_expression:
-    print(iflow)
-    sched = ExpressionSchedular(iflow["cron"], qt = datetime.date(2023,10,26))
-    list.append([i.strftime("%H:%M") for i in sched.get_schedule_timetable()])
-with open('output.txt', 'w') as f:
-    for line in list:
-        f.write("%s\n" % line)
+
 # GetDescription uses DescriptionTypeEnum.FULL by default:
 
 # Or passing Options class as second argument:
@@ -30,5 +25,7 @@ with open('output.txt', 'w') as f:
 # descriptor = ExpressionDescriptor("0 23 ? * MON-FRI", options)
 # #parser = ExpressionParser("*/10 * * * *", options)
 #print(descriptor.get_description(DescriptionTypeEnum.FULL))
+
+
 
 
